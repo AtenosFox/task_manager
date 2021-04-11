93755ed05c300b0df6fac027d71708f91054e63d
Artem <56757769+ShatkovAI@users.noreply.github.com>
2021-04-11 17:31:30 +0300

    initial commit

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-b-b_=d7n=76s2dbeeyd@&ra7ap&vjmq4duzj2ob$p_#m11-2pb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
