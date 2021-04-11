02d188b8203dd925fc8c432d024ed3d02a7c5924
Artem <56757769+ShatkovAI@users.noreply.github.com>
2021-04-11 23:53:42 +0300

    initial commit

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)  # Поле с размером до 255 символов. В данном случае не больше 50
+    task = models.TextField('Описание')  # Поле с размером более 255 символов
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
